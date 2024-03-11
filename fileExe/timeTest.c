81c1ba00807ea4d8645d00eca6146d3dbb9c36e2
Luckyidiot <tranleanhquan6309@gmail.com>
2024-03-11 14:20:24 -0400

    File_Naming mechanics is done

--- fileExe/timeTest.c
+++ fileExe/timeTest.c
@@ -3,13 +3,13 @@
 #include <time.h>
 #include <string.h>
 
-void File_Naming(char* fileName){
+void File_Naming(char* filename, const char* IPaddr){
     /**
      * Each client will have an execution file
      * Those files will be named using their IP address and Date & Time to ensure the uniqueness
     */
     time_t rawTime = time(NULL);
-    sprintf(filename, rawTime)
+    sprintf(filename, "%s_%ld", IPaddr, rawTime);
     
 }
 
@@ -24,6 +24,8 @@ int main(int argc, char** argv){
     printf("Formatter: %s\n", formattedTime);
 
     char* IPaddr = "127.0.0.1";
-    printf("%ld\n",strlen(IPaddr));
+    File_Naming(filename, IPaddr);
+
+    printf("The filename is: %s\n", filename);
     return 0;
 }
